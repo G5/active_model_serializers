c710df9df269a88175a45f232eb24c68712157a1
Ramon Tayag <ramon.tayag@gmail.com>
2015-07-15 13:23:25 +0800

    Do not override render method

--- lib/active_model_serializers_9.rb
+++ lib/active_model_serializers_9.rb
@@ -6,15 +6,18 @@ require 'active_model/serializer_9/railtie' if defined?(Rails)
 
 begin
   require 'action_controller'
-  require 'action_controller/serialization_9'
-  require 'action_controller/serialization_test_case'
+  # NOTE Do not override the render method, since this gem is meant to work with
+  # another version of AMS that already / may already override it.
+  #
+  # require 'action_controller/serialization_9'
+  # require 'action_controller/serialization_test_case'
 
-  ActiveSupport.on_load(:action_controller) do
-    if ::ActionController::Serialization9.enabled
-      ActionController::Base.send(:include, ::ActionController::Serialization9)
-      ActionController::TestCase.send(:include, ::ActionController::SerializationAssertions)
-    end
-  end
+  # ActiveSupport.on_load(:action_controller) do
+  #   if ::ActionController::Serialization9.enabled
+  #     ActionController::Base.send(:include, ::ActionController::Serialization9)
+  #     ActionController::TestCase.send(:include, ::ActionController::SerializationAssertions)
+  #   end
+  # end
 rescue LoadError
   # rails not installed, continuing
 end
